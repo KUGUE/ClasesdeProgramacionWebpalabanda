d79b5884af49baca88f2ef76ad5c766f400c628f
KUGUE <martin-kugue@hotmail.com>
2023-06-25 00:33:07 -0700

    Proyecto Chat - Implemente uno de mis proyectos finales para trabajarlo y mejorarlo

---
+++ Chat/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - youtube.com/codingnepal -->
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Chat De Amigos</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
+
+  <style>
+  .wrapper{
+  background: #fff;
+  max-width: 450px;
+  width: 100%;
+  border-radius: 16px;
+  box-shadow: 0 0 128px 0 rgba(0,0,0,0.1),
+              0 32px 64px -48px rgba(0,0,0,0.5);
+}
+</style>
+
+</head>
